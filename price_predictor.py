08b4787f59c5ccf3aefa201659b60a553aaeb39d
atmac0 <ilovemrknets@gmail.com>
2020-06-27 14:14:52 -0400

    added stuff for RNN

--- price_predictor.py
+++ price_predictor.py
@@ -2,28 +2,78 @@ import tensorflow as tf
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
 import seaborn as sns
-#%matplotlib inline
 
-df = pd.read_csv('data/rune_data.csv')
-df.set_index('timestamp')
-df.head()
+def univariate_data(dataset, start_index, end_index, history_size, target_size):
+    data = []
+    labels = []
 
-print(df.describe())
-print(df.shape)
+    start_index = start_index + history_size
 
-ax = df.hist(bins=30)
-#plt.show()
+    if end_index is None:
+        end_index = len(dataset) - target_size
 
-sns.boxplot(x=df['Chaos_rune'])
+    for i in range(start_index, end_index):
+        indices = range(i - history_size, i)
 
-plt.show()
+        data.append(np.reshape(dataset[indices], (history_size, 1)))
+        labels.append(dataset[i + target_size])
+    return np.array(data), np.array(labels)
+    
 
-plt.figure(figsize=(20,10))
-c = df.corr()
-sns.heatmap(c,annot=True)
+def standardize(uni_data, TRAIN_SPLIT):
 
-train_ratio = 0.8 # percent of dataset used for training
+    uni_train_mean = uni_data[:TRAIN_SPLIT].mean()
+    uni_train_std  = uni_data[:TRAIN_SPLIT].std()
+
+    uni_data = (uni_data - uni_train_mean) / uni_train_std
+    
+    return uni_data
+
+def baseline(history):
+  return np.mean(history)
 
 
+def main():
 
+    df = pd.read_csv('data/rune_data.csv')
+    df.set_index('timestamp')
+
+    tf.random.set_seed(13)
+    
+    univariate_past_history    = 20
+    univariate_future_target   = 0
+
+    BATCH_SIZE  = 20
+    BUFFER_SIZE = 5
+    
+    uni_data       = df['Chaos_rune']
+    uni_data.index = df['timestamp']
+
+    uni_data = uni_data.values
+    
+    TRAIN_SPLIT = int(0.8 * len(uni_data)) # percent of dataset used for training
+    
+    uni_data = standardize(uni_data, TRAIN_SPLIT)
+
+    x_train_uni, y_train_uni = univariate_data(uni_data, 0, TRAIN_SPLIT, univariate_past_history, univariate_future_target)
+    x_val_uni, y_val_uni     = univariate_data(uni_data, TRAIN_SPLIT, None, univariate_past_history, univariate_future_target)
+    
+    train_univariate = tf.data.Dataset.from_tensor_slices((x_train_uni, y_train_uni))
+    train_univariate = train_univariate.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
+
+    val_univariate = tf.data.Dataset.from_tensor_slices((x_val_uni, y_val_uni))
+    val_univariate = val_univariate.batch(BATCH_SIZE).repeat()
+
+    simple_lstm_model = tf.keras.models.Sequential([
+        tf.keras.layers.LSTM(8, input_shape=x_train_uni.shape[-2:]),
+        tf.keras.layers.Dense(1)
+        ])
+
+    simple_lstm_model.compile(optimizer='adam', loss='mae')
+
+    
+                                                        
+if(__name__ == "__main__"):
+    main()
