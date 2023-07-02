9c19c66f35a0361176378a01ea0278b823053c06
atmac0 <ilovemrknets@gmail.com>
2023-07-02 17:23:07 -0400

    added changes

--- price_predictor.py
+++ price_predictor.py
@@ -1,9 +1,16 @@
 import tensorflow as tf
+
 import pandas as pd
 import numpy as np
+
 import matplotlib.pyplot as plt
-import matplotlib
+import matplotlib as mpl
 import seaborn as sns
+
+import os
+
+def multivariate_data(dataset, start_index, end_index, history_size):
+    print(dataset)
 
 def univariate_data(dataset, start_index, end_index, history_size, target_size):
     data = []
@@ -45,15 +52,24 @@ def main():
     univariate_past_history    = 20
     univariate_future_target   = 0
 
+    multi_past_history    = 20
+    multi_future_target   = 0
+    
     BATCH_SIZE  = 20
     BUFFER_SIZE = 5
-    
+
     uni_data       = df['Chaos_rune']
     uni_data.index = df['timestamp']
 
     uni_data = uni_data.values
+
     
-    TRAIN_SPLIT = int(0.8 * len(uni_data)) # percent of dataset used for training
+    multi_data       = df
+    multi_data.index = df['timestamp']
+
+    multi_data = multi_data.values
+    
+    TRAIN_SPLIT = int(0.8 * len(multi_data)) # percent of dataset used for training
     
     uni_data = standardize(uni_data, TRAIN_SPLIT)
 
@@ -68,12 +84,33 @@ def main():
 
     simple_lstm_model = tf.keras.models.Sequential([
         tf.keras.layers.LSTM(8, input_shape=x_train_uni.shape[-2:]),
-        tf.keras.layers.Dense(1)
+         tf.keras.layers.Dense(1)
         ])
 
     simple_lstm_model.compile(optimizer='adam', loss='mae')
 
+    EVALUATION_INTERVAL = 200
+    EPOCHS = 10
+
+    print(type(train_univariate))
+    ''' simple_lstm_model.fit(train_univariate, epochs=EPOCHS,
+                          steps_per_epoch=EVALUATION_INTERVAL,
+                          validation_data=val_univariate, validation_steps=50)
+
     
+    
+    future_target = 72
+    print(multi_data)
+    print(multi_data.shape)
+    print(multi_data[:, 1])
+    x_train_multi, y_train_multi = multivariate_data(multi_data, multi_data[:, 1], 0,
+                                                     TRAIN_SPLIT, multi_past_history,
+                                                     multi_future_target)
+    x_val_multi, y_val_multi = multivariate_data(multi_data, multi_data[:, 1],
+                                                 TRAIN_SPLIT, None, multi_past_history)'''
+
+
+ 
                                                         
 if(__name__ == "__main__"):
     main()
